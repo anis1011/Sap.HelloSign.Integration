93b0ea2dbfdb4d4c6669c95f9d325b24063b6f99
anis1011 <maharzn11@gmail.com>
2021-05-25 10:27:52 +0545

    addon for payment voucher added

---
+++ SPC_B1HelloSignAddOn/Helpers/Menu.cs
@@ -0,0 +1,23 @@
+﻿using SPC.MainLibrary.SAPB1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SAPbobsCOM;
+
+namespace SPC_B1HelloSignAddOn.Helpers
+{
+    public class Menu
+    {
+        public static void addMenu()
+        {
+            B1Helper.addMenuItem("43520", "Payment Voucher", "Payment Voucher");
+            //B1Helper.AddSubMenu(MenuID_UD.MODULE, "Gate Pass master", "Gate Pass", -1, string.Concat(System.Windows.Forms.Application.StartupPath, @"\Images\Icon.png"));
+        }
+        public static void RemoveMenu()
+        {
+            //B1Helper.RemoveMenuItem("3072", "Auto Item Master Code Setup", "Auto Item Master Code Setup");
+        }
+    }
+}
---
+++ SPC_B1HelloSignAddOn/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using SAPbouiCOM.Framework;
+using SPC_B1HelloSignAddOn.Helpers;
+using SAPbobsCOM;
+using System.Configuration;
+using SPC.MainLibrary.Utilities;
+
+namespace SPC_B1HelloSignAddOn
+{
+    class Program
+    {
+        #region Variables
+
+        public static SAPbobsCOM.Company oCompany;
+        public static SAPbouiCOM.Application SBO_Application;
+        public static SAPbouiCOM.Form oForm { get; set; }
+
+        #endregion
+
+        /// <summary_>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            try
+            {
+                Application oApp = null;
+                if (args.Length < 1)
+                    oApp = new Application();
+                else
+                    oApp = new Application(args[0]);
+
+                SBO_Application = Application.SBO_Application;
+                oCompany = (Company)SBO_Application.Company.GetDICompany();
+                Menu.addMenu();
+
+                if (ConfigurationManager.AppSettings["UDF"].ToString() == "N")
+                {
+                    AddonInfoInfo.InstallUDOs();
+                }
+                //AddonInfoInfo.GetCommonSettings();
+                var applicationHandler = new ApplicationHandlers();
+                Application.SBO_Application.StatusBar.SetSystemMessage("Add-on installed successfully.", SAPbouiCOM.BoMessageTime.bmt_Short, SAPbouiCOM.BoStatusBarMessageType.smt_Success);
+                oApp.Run();
+
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
